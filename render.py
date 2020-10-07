7f78ae7ff54ecbccd90d333bb2090384fcbabea3
John <johnsutor3@gmail.com>
2020-10-07 14:21:17 -0400

    Amend render script for Linux

--- render.py
+++ render.py
@@ -6,7 +6,7 @@ Script for generating many synthetic images
 '''
 import os 
 
-BLENDER_DIR = "C:\Program Files\Blender Foundation\Blender 2.83"
+BLENDER_DIR = "/home/mlpc/blender/blender-2.83.2-linux64"
 NUM_RENDERS =  3 
 
 # Determine the working directory 
@@ -15,4 +15,4 @@ WORK_DIR = os.getcwd()
 # Change to blender and run the render script 
 os.chdir(BLENDER_DIR)
 for i in range(NUM_RENDERS):
-    os.system(".\\blender -b --python " + WORK_DIR + "\\synthblend.py -- -w " + WORK_DIR + " -rc " + str(i + 1) + " -bb YOLO -ra 3 -pmin 1.0471975512")
+    os.system("blender -b --python " + WORK_DIR + "/synthblend.py -- -w " + WORK_DIR + " -rc " + str(i + 1) + " -bb YOLO -ra 3 -pmin 1.0471975512")
