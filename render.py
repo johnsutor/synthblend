dd39733b9ae11b8cc068f28b7882068242521369
John Sutor <johnsutor3@gmail.com>
2021-01-05 09:55:45 -0500

    Provide new default temporary directory for Joblib

--- render.py
+++ render.py
@@ -15,7 +15,7 @@ import random
 from joblib import Parallel, delayed
 
 BLENDER_DIR = "C:\Program Files\Blender Foundation\Blender 2.83"
-NUM_RENDERS =  5
+NUM_RENDERS =  5000
 
 # Determine the working directory 
 WORK_DIR = os.getcwd()
@@ -25,7 +25,7 @@ render = lambda rc: os.system("blender -b --python " + WORK_DIR + "/synthblend.p
 
 # Change to blender and run the render script 
 os.chdir(BLENDER_DIR)
-Parallel(n_jobs=-1)(delayed(render)(i) for i in range(NUM_RENDERS))
+Parallel(n_jobs=-1, temp_folder='/tmp')(delayed(render)(i) for i in range(NUM_RENDERS))
 
 def apply_background(img):
     if not '.txt' in img:  
@@ -61,41 +61,6 @@ def apply_background(img):
 
         imsave(WORK_DIR + '/renders/' + img, image["image"])
 
-Parallel(n_jobs=-1)(
+Parallel(n_jobs=-1, temp_folder='/tmp')(
     delayed(apply_background)(img) for img in os.listdir(WORK_DIR + "/renders/")
 )
-
-# # Add a background to each of the renders
-# for img in os.listdir(WORK_DIR + '/renders/'):
-#     if not '.txt' in img:  
-#         # Import the render
-#         render = Image.open(WORK_DIR + '/renders/' + img).convert("RGBA")
-#         rw, rh = render.size 
-
-#         # Choose a random background 
-#         background = random.choice(os.listdir(WORK_DIR + '/backgrounds/'))
-#         background = Image.open(WORK_DIR + '/backgrounds/' + background).convert("RGBA")
-#         bw, bh = render.size
-
-#         # Resize the background to match the render 
-#         background = background.resize((rw, rh))
-
-#         # Center crop the background based on the render size
-#         # background = background.crop(((bw - rw) / 2, (bh - rh)/2, (bw + rw)/2, (bh + rh)/2))
-        
-#         # Merge the background and the render 
-#         background.paste(render, (0,0), mask=render)
-#         background.save(WORK_DIR + '/renders/' + img)
-
-#         # Set the image transforms
-#         transforms = albumentations.Compose([
-#             GaussianBlur(blur_limit=(3,5)),
-#             IAAAdditiveGaussianNoise()
-#         ])
-
-#         # Apply the Gaussian Filter to the image 
-#         image = imread(WORK_DIR + '/renders/' + img)
-        
-#         image = transforms(image=image)
-
-#         imsave(WORK_DIR + '/renders/' + img, image["image"])
